e1726c23e245842a8c7db753fe4e1fe86cd39211
Replit user <>
2024-01-10 20:58:09 +0000

    Basics with scheme

---
+++ scheme/functions.scm
@@ -0,0 +1,14 @@
+(define (contains aVal aList)
+	(if (null? aList) #f ; base case
+			(if (eq? aVal (car aList)) ; is 1st element it
+					#t ; then true
+					(contains aVal (cdr aList))) ; else check rest of list recursively
+			))
+
+(display (contains 2 '(1 2 3)))
+(newline)
+(display (contains 4 '(1 2 3)))
+(newline)
+(display (if (contains 2 '(1 2 3)) "yes" "no"))
+(newline)
+(display (if (contains 4 '(1 2 3)) "yes" "no"))
