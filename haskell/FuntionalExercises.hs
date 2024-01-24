371fc16660033fb69fe1ba82f4f6e5931dc70314
Xavier Silva <Xavier.Silva20@kzoo.edu>
2024-01-24 15:31:55 -0500

    Update comments for exercises

--- haskell/FuntionalExercises.hs
+++ haskell/FuntionalExercises.hs
@@ -2,7 +2,7 @@ module FunctionalExercises where
 import TestSuiteSupportModule
 
 
--- Exercise 1: `contains` function f(x, y) == True if x in list y
+-- Exercise 1: function `contains(x, y)` is True if x in list y
 contains :: Int -> [Int] -> Bool
 contains _ [] = False
 contains x lst
@@ -18,7 +18,7 @@ containsTestSuite =
     Test "contains 2 [1, 2, 3, 2]" (contains 2 [1, 2, 3, 2] == True)
   ]
 
--- Exercise 2: `removeAll` function f(l, x) returns list with all instances of x removed
+-- Exercise 2: function `removeAll(l, x)` returns list l with all instances of x removed
 removeAll :: [Int] -> Int -> [Int]
 removeAll [] _ = []
 removeAll lst x
@@ -34,7 +34,7 @@ removeAllTestSuite =
     Test "removeAll [2, 1, 3, 1] 1" (removeAll [2, 1, 3, 1] 1 == [2, 3])
   ]
 
--- Exercise 3: `deleteList` function f(l1, l2) returns list with all instances of l2
+-- Exercise 3: `deleteList(l1, l2)` returns list l1 with all instances of l2 deleted
 deleteList :: [[Int]] -> [Int] -> [[Int]]
 deleteList [] _ = []
 deleteList lst x
@@ -47,11 +47,11 @@ deleteListTestSuite =
     Test "deleteList [[1,2],[1,3],[1,2]] [1,2]" (deleteList [[1,2],[1,3],[1,2]] [1,2] == [[1,3]])
   ]
 
--- Exercise 4: `removeAllFromList` function f(l1, l2) returns list with all elements of l2 removed
+-- Exercise 4: `removeAllFromList(l1, l2)` returns list l1 with all elements of l2 removed
 removeAllFromList :: [Int] -> [Int] -> [Int]
 removeAllFromList [] _ = []
 removeAllFromList lst x
-  | head lst `contains` x = removeAllFromList (tail lst) x
+  | contains (head lst) x = removeAllFromList (tail lst) x
   | otherwise = head lst : removeAllFromList (tail lst) x
 
 removeAllFromListTestSuite =
@@ -63,7 +63,7 @@ removeAllFromListTestSuite =
     Test "removeAllFromList [1,4,2,3,2,1,4] [1,2]" (removeAllFromList [1,4,2,3,2,1,4] [1,2] == [4,3,4])
   ]
 
--- Exercise 5: `deleteLast` function f(l) returns list with last element of l removed
+-- Exercise 5: `deleteLast(l)` returns list l with last element of l removed
 deleteLast :: [Int] -> [Int]
 deleteLast [] = []
 deleteLast [x] = []
@@ -78,7 +78,7 @@ deleteLastTestSuite =
     Test "deleteLast [1, 2, 1]" (deleteLast [1, 2, 1] == [1, 2])
   ]
 
--- Exercise 6: reverse list
+-- Exercise 6: function `reverseList(lst)` returns the reverse of list lst
 reverseList :: [Int] -> [Int]
 reverseList [] = []
 reverseList lst = reverseList (tail lst) ++ [head lst]
@@ -92,7 +92,7 @@ reverseListTestSuite =
     Test "reverseList [1,2,3,4,5]" (reverseList [1,2,3,4,5] == [5,4,3,2,1])
   ]
 
--- Exercise 7: list min and max
+-- Exercise 7: function `lstMinMax(lst)` returns a tuple (min, max) where min is the minimum of lst and max is the maximum of lst
 lstMin :: [Int] -> Int
 lstMin [x] = x
 lstMin [x, y]
